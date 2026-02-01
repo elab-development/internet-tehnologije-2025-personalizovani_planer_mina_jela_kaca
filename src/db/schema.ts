85fc01833c2ad79cf161dbce96f5654f1979b832
Katarina Jokanovic <kaca.2002.kj@gmail.com>
2026-02-01 20:55:26 +0100

    Kostur baze

---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, varchar, uuid, timestamp } from "drizzle-orm/pg-core";
+
+export const korisnici = pgTable("korisnici", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    username: varchar("username", {length: 100}).notNull(),
+    email: varchar("email", {length: 255}).notNull().unique(),
+    ime: varchar("ime", {length: 100}).notNull(),
+    prezime: varchar("prezime", {length: 100}).notNull(),
+    adresa: varchar("adresa", {length: 100}).notNull(),
+    passHash: varchar("pass_hash", {length: 255}).notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+});
---
+++ src/db/seed.ts
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import { korisnici  } from "./schema";
+import { db } from "./index";
+import bcrypt from "bcrypt";
+
+const hash = await bcrypt.hash("1233", 10);
+
+await db.transaction(async (tx) => {
+    await tx.insert(korisnici).values([
+    {
+	    username:"mau",
+	    email:"mau@gmail.com",
+	    ime:"Mau",
+	    prezime:"Maunovic",
+	    adresa:"ulica mau",
+	    passHash:hash
+    },
+    {
+	    username:"kaca",
+	    email:"kaca@gmail.com",
+	    ime:"Kaca",
+	    prezime:"Kacinovic",
+	    adresa:"ulica kaca",
+	    passHash:hash
+    },
+    {
+	    username:"mina",
+	    email:"mina@gmail.com",
+	    ime:"Mina",
+	    prezime:"Minic",
+	    adresa:"ulica mina",
+	    passHash:hash
+    },
+]);
+});
+
+process.exit(0);
