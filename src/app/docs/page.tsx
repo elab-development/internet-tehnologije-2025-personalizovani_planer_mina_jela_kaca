d920db2ffabbb5c87789e40f781a7cf1d3dc41c3
Katarina Jokanovic <kaca.2002.kj@gmail.com>
2026-03-01 15:59:54 +0100

    Swagger-api zahtevi

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+
+export default function ReactSwagger({ spec }: { spec: any }) {
+  return <SwaggerUI url='swagger.yaml'/>;
+}
