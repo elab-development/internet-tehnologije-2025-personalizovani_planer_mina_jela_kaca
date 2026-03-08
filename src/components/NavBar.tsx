53120688f26f1642e747a2536d70ff38f990935c
Mina Lazovic <minalazovic2002@gmail.com>
2026-03-08 22:54:53 +0100

    dodate macmac slike, eksterni api =^.,.^=

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import {useAuth} from "@/components/AuthProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RiUser3Line } from "@remixicon/react";
 import { useRouter } from "next/navigation";
  
@@ -16,6 +16,19 @@ export function NavBar (){
     const isAdmin = status === "autentifikovani" && user?.uloga === "admin";
     const [open, setOpen] = useState(false); //prozor profila
     
+    // učitavanje mačke sa Cataas API
+    const [cat, setCat] = useState("");
+    const [catOpen, setCatOpen] = useState("");
+    useEffect(() => {
+        fetch("https://cataas.com/cat?json=true")
+            .then(res => res.json())
+            .then(data => {
+                setCat(`https://cataas.com/cat/${data.id}`);
+                setCatOpen(`https://cataas.com/cat/${data.id}?width=200&height=200`);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
     const router = useRouter();
 
     const handleLogout = async () => {
@@ -51,13 +64,29 @@ export function NavBar (){
                             className="flex h-7 w-7 items-center cursor-pointer justify-center rounded-full bg-purple-100 text-purple-600 transition hover:bg-pink-200"
                             onClick={() => setOpen(prev => !prev)}
                         >   
-                            <RiUser3Line className="h-4 w-4" />
+                            {/*//<RiUser3Line className="h-4 w-4" />*/}
+                             {cat ? (
+                                <img
+                                    src={cat}
+                                    alt="mac mac"
+                                    className="h-8 w-8 object-cover rounded-full"
+                                />
+                            ) : (
+                                <div className="h-4 w-4 bg-pink-300 rounded-full animate-pulse" />
+                            )}
                         </button>
 
                         <div
                             className={`z-50  absolute right-0 top-12  min-w-47.5 rounded-md border border-black/10 
                                 bg-white p-2 text-sm shadow-md transition-all ${open ? "opacity-100" : "opacity-0 pointer-events-none"}`}
-                        >
+                        >   
+                            {catOpen && (
+                                <img
+                                    src={catOpen}
+                                    alt="Mac mac"
+                                    className="w-32 h-32 object-cover rounded-md mx-auto mb-2"
+                                />
+                            )}
                             <span className="block px-2 py-1 text-purple-700 max-w-35 text-nowrap overflow-hidden ">
                                 {user?.ime}{" ["}{user?.username}{"]"}
                             </span>
